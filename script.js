585a65e0c190fc5be069601a020603b8b372c767
James Taylor <james@jtaylor.id.au>
2021-09-10 20:11:44 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ $.get('https://ip.youhacked.me/cdn-cgi/trace', function(data) {
 const canvas = document.getElementById('canv');
 const ctx = canvas.getContext('2d');
 
-const w = canvas.width = document.body.offsetWidth;
-const h = canvas.height = document.body.offsetHeight;
+const w = canvas.width = canvas.clientWidth;
+const h = canvas.height = canvas.clientHeight
 const cols = Math.floor(w / 20) + 1;
 const ypos = Array(cols).fill(0);
 
@@ -31,7 +31,7 @@ function matrix () {
   ctx.fillRect(0, 0, w, h);
   
   ctx.fillStyle = '#0f0';
-  ctx.font = '15pt monospace';
+  ctx.font = '1vw monospace';
   
   ypos.forEach((y, ind) => {
     const text = String.fromCharCode(Math.random() * 128);
@@ -42,4 +42,4 @@ function matrix () {
   });
 }
 
-setInterval(matrix, 50);
+setInterval(matrix, 100);
