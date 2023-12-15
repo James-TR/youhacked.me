be3aee57fa18b22d29fe5a71772cd1206d9faf39
James Taylor <james@jtaylor.id.au>
2023-12-15 18:24:55 +1100

    Update code matrix

--- script.js
+++ script.js
@@ -14,32 +14,106 @@ $.get('https://ip.youhacked.me/cdn-cgi/trace', function(data) {
 }( document.title + " - I know your IP address - " + data.ip + " - You can't hide forever - "));
 });
 
-
-const canvas = document.getElementById('canv');
+const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const w = canvas.width = canvas.clientWidth;
-const h = canvas.height = canvas.clientHeight
-const cols = Math.floor(w / 20) + 1;
-const ypos = Array(cols).fill(0);
+// let gradient = ctx.createLinearGradient(0, canvas.height, canvas.width, 0);
+let gradient = ctx.createRadialGradient(canvas.width / 2, canvas.height / 2, 100, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+gradient.addColorStop(0, 'red');
+gradient.addColorStop(0.2, 'yellow');
+gradient.addColorStop(0.4, 'green');
+gradient.addColorStop(0.5, 'cyan');
+gradient.addColorStop(0.8, 'blue');
+gradient.addColorStop(1, 'magenta');
 
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, w, h);
+class Symbol {
+    constructor(x, y, fontSize, canvasHeight) {
+        // this.characters = 'Prince';
+        // this.characters = '❤';
+        // this.characters = '404 not found';
+        // this.characters = '☀☁❆WEATHER❅❄';
+        // this.characters = '♔♕♖♗♘♙CHESS♚♛♜♝♞♟';
+        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		// this.characters = 'YOU HACKED ME'
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = '';
+        this.canvasHeight = canvasHeight
 
-function matrix () {
-  ctx.fillStyle = '#0001';
-  ctx.fillRect(0, 0, w, h);
-  
-  ctx.fillStyle = '#0f0';
-  ctx.font = '1vw monospace';
-  
-  ypos.forEach((y, ind) => {
-    const text = String.fromCharCode(Math.random() * 128);
-    const x = ind * 20;
-    ctx.fillText(text, x, y);
-    if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
-    else ypos[ind] = y + 20;
-  });
+    }
+    draw(context) {
+        this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+        // context.fillStyle = '#0aff0a';
+        // context.fillStyle = 'red';
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+            this.y = 0;
+
+        } else {
+            this.y += 1;
+        }
+    }
 }
 
-setInterval(matrix, 100);
+
+class Effect {
+    constructor(canvasWidth, canvasHeight) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontSize = 25;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+        console.log(this.symbols);
+    }
+    #initialize() {
+        for (let i = 0; i < this.columns; i++) {
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+        }
+    }
+    resize(width, height) {
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+    }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 160;
+const nextFrame = 1000 / fps;
+let timer = 0;
+
+
+
+function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if (timer > nextFrame) {
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        ctx.textAlign = 'center';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        // ctx.fillStyle =  '#0aff0a';
+        ctx.fillStyle = gradient;
+        ctx.font = effect.fontSize + 'px monospace';
+        effect.symbols.forEach(symbol => symbol.draw(ctx));
+
+        timer = 0;
+    }
+    else {
+        timer += deltaTime;
+    }
+    requestAnimationFrame(animate);
+}
+animate(0);
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+})
